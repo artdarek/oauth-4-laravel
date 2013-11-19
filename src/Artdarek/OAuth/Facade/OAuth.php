3789584103ea62844583cc30cd24e25ce805ea63
Dariusz Prząda <dariuszp@mawelous.com>
2013-11-19 14:03:50 +0100

    Description added

--- src/Artdarek/OAuth/Facade/OAuth.php
+++ src/Artdarek/OAuth/Facade/OAuth.php
@@ -1,8 +1,16 @@
-<?php namespace Artdarek\OAuth\Facade;
+<?php 
+/**
+ * @author     Dariusz Prząda <artdarek@gmail.com>
+ * @copyright  Copyright (c) 2013
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+
+namespace Artdarek\OAuth\Facade;
 
 use Illuminate\Support\Facades\Facade;
 
-class OAuth extends Facade {
+class OAuth extends Facade 
+{
 
     /**
      * Get the registered name of the component.
